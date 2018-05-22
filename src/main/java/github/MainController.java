4322c5a0e25f647502204e096fd5b3d73b4eef11
Frank Di <frankker@gmail.com>
2018-05-23 01:50:32 +0200

    first push

---
+++ src/main/java/github/MainController.java
@@ -0,0 +1,15 @@
+package github;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+	@RequestMapping("/")
+	public String mainPage(){
+		
+		return "Hello World";
+		
+	}
+}

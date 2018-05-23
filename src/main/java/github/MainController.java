deba5825c79f1eb1f6af8fe9a585f2cb80f7775f
Frank Di <frankker@gmail.com>
2018-05-23 02:39:45 +0200

    frankker push

--- src/main/java/github/MainController.java
+++ src/main/java/github/MainController.java
@@ -9,7 +9,7 @@ public class MainController {
 	@RequestMapping("/")
 	public String mainPage(){
 		
-		return "Hello World! A nice day!";
+		return "Hello World! A nice day! By frankker!";
 		
 	}
 }
